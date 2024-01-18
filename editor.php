98103029b42144d2cfb1038ea8049bc0238d5554
benjohnsto <benj@princeton.edu>
2024-01-18 06:12:00 -0500

    function name conflict with layered reading

--- editor.php
+++ editor.php
@@ -8,31 +8,31 @@
 // https://www.gavick.com/blog/wordpress-tinymce-custom-buttons
 
 
-function linguaviva_add_tinymce_plugin($plugin_array) {
-    $plugin_array['linguaviva_tc_button'] = plugins_url( 'js/editor.js', __FILE__ );
+function textbook_add_tinymce_plugin($plugin_array) {
+    $plugin_array['textbook_tc_button'] = plugins_url( 'js/editor.js', __FILE__ );
     return $plugin_array;
 }
 
 
 
 
-function linguaviva_register_my_tc_button($buttons) {
+function textbook_register_my_tc_button($buttons) {
    array_push($buttons, "text","textarea", "sentence","radiobuttons","checkboxes","dropdown","googleimages","priberam");
    return $buttons;
 }
 
 
 
-function linguaviva_add_my_tc_button() {
+function textbook_add_my_tc_button() {
     global $typenow;
 
     // check user permissions
     if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
     return;
     }
-    add_filter("mce_external_plugins", "linguaviva_add_tinymce_plugin");
-    add_filter('mce_buttons', 'linguaviva_register_my_tc_button');
+    add_filter("mce_external_plugins", "textbook_add_tinymce_plugin");
+    add_filter('mce_buttons', 'textbook_register_my_tc_button');
 
 }
 
-add_action('admin_head', 'linguaviva_add_my_tc_button');
+add_action('admin_head', 'textbook_add_my_tc_button');
